2c5aa28e8deafb11c0cfa0e5a65edc939916f4c6
Md Tarikul Islam <125537691+Tarikulat@users.noreply.github.com>
2024-04-24 17:45:06 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
-
+// */
+// user
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
